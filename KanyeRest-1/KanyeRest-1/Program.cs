014bc63f8f5e0ed479eca7b956a68e1b10c56716
Taylor Savage <taylorsavage93@gmail.com>
2022-08-03 10:11:15 -0400

    finished exercise 1

---
+++ KanyeRest-1/KanyeRest-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using KanyeRest_1;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+
+{
+    var client = new HttpClient();
+
+    var quote = new QuoteGenerator(client);
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("------------------------");
+        Console.WriteLine($"Kanye: {quote.Kanye()}");
+
+        Console.WriteLine($"Ron Swanson: {quote.RonSwanson()}");
+    }
+
+
+}
